049f2b8095ba9abacbaae424fe72ca777afd317f
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2023-05-21 04:08:47 +0500

    Create force_sub.py

---
+++ bot/force_sub.py
@@ -0,0 +1,32 @@
+from telethon import TelegramClient, events, functions, types
+
+# Your bot credentials
+api_id = YOUR_API_ID
+api_hash = 'YOUR_API_HASH'
+bot_token = 'YOUR_BOT_TOKEN'
+
+# Your channel username
+channel_username = 'YOUR_CHANNEL_USERNAME'
+
+# Create a Telethon client
+client = TelegramClient('my_bot', api_id, api_hash).start(bot_token=bot_token)
+
+# Define a handler for incoming messages
+@client.on(events.NewMessage(func=lambda event: not event.is_private))
+async def my_handler(event):
+    # Get the user ID
+    user_id = event.message.from_id
+
+    # Check if the user is already subscribed to the channel
+    result = await client(functions.channels.GetFullChannelRequest(channel=channel_username))
+    if user_id not in [user.user_id for user in result.full_chat.participants]:
+        # Force the user to subscribe to the channel
+        try:
+            await client(functions.channels.JoinChannelRequest(channel=channel_username))
+            await client.send_message(user_id, 'You have been subscribed to the channel.')
+        except Exception as e:
+            print(e)
+            await client.send_message(user_id, 'An error occurred while subscribing to the channel.')
+
+# Start the event loop
+client.run_until_disconnected()
